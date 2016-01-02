f317a765e66c6ac426958114d74722f2fb8b8e59
Jakub Mach <jakub.m4ch@gmail.com>
2016-01-02 06:37:22 +0100

    shit broke sleep time

--- mpchc.py
+++ mpchc.py
@@ -3,8 +3,8 @@
 import sys, os, requests, argparse, json
  
 def main(command, base_url):
-	base_url = base_url or "http://192.168.0.105:3333" # temporary
 	cmd = command  # temporary
+	print("CMD is", ord(cmd))
 	cmds = {"p": 889, "n": 922, "b": 921, "f": 830, "jf": 900, "jb": 899}
 	rcmd = cmds[cmd]
 	rdata = {"wm_command": rcmd}
@@ -16,11 +16,24 @@ def main(command, base_url):
 		print("Unknown error.")
 
 if __name__ == "__main__":
+	# todo: config parse (baseurl + command prefs)
+	# todo: cleanup
+	# todo: optional curses
 	aparser = argparse.ArgumentParser()
-	aparser.add_argument("--url", help="base url on which mpc-hc web iface is")
+	aparser.add_argument("--url", help="Base url on which mpc-hc web iface is")
+	aparser.add_argument("--ui", help="Available options: none (default)|text|curses")
 	aparser.add_argument("command")
 	args = aparser.parse_args()
 	if args.url:
-		main(args.command, args.url)
+		if args.ui == "text":
+			while True:
+				acmd = input("Type a command or 'quit' and press Enter: ")
+				if acmd != "quit":
+					main(str(acmd), args.url)
+				else:
+					sys.exit(0)
+		
+		else:
+			main(args.command, args.url)
 	else:
-		main(args.command, "")
+		print("You need to specify a URL of the remote web interface using the --url argument.")
