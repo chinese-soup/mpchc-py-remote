04b715b20fe405981fc8ebecc224679ea7fda7d6
Jakub Mach <jakub.m4ch@gmail.com>
2016-01-13 19:56:24 +0100

    typo

--- mpchc.py
+++ mpchc.py
@@ -52,7 +52,6 @@ if __name__ == "__main__":
 
 	#if args.ip:
 	# TODO: FIX
-	print("ARGS = ", args)
 	if args.ui == "text":
 		print("Type either a command or 'quit' and submit using Enter.")
 		while True:
@@ -70,7 +69,7 @@ if __name__ == "__main__":
 				sys.exit(0)
 	
 	elif args.command is None:
-		print("You didn't specify a command. Did you mean to run in an interactive mode? Run with \"-i text\" or \"-i curses\".")
+		print("You didn't specify a command. Did you mean to run in an interactive mode? If so, run with \"-i text\" or \"-i curses\".")
 		sys.exit(1)
 	elif args.ui is None or args.ui == "none" and args.command is not None:
 		main(args.command, args.ip or config.settings["ip"], args.port or config.settings["port"])
